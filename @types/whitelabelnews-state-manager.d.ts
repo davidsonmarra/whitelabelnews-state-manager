12673ca82886e57be5dfda198175b19f55e65b23
Davidson Marra <davidsonmarra@CPU-00335.local>
2023-03-13 12:23:12 -0300

    chore: att types

--- @types/whitelabelnews-state-manager.d.ts
+++ @types/whitelabelnews-state-manager.d.ts
@@ -2,7 +2,7 @@ import {
   store as exportedStore,
   IRootState as exportedIRootState,
   FETCH_NEWS as exportedFETCH_NEWS,
-} from './src';
+} from '../src';
 declare module 'whitelabelnews-state-manager' {
   export const store: typeof exportedStore;
   export type IRootState = exportedIRootState;
