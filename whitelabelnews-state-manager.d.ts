315bd71a59cab94bf17bb44426e9265efdaebfa3
Davidson Marra <davidsonmarra@CPU-00335.local>
2023-03-13 12:14:15 -0300

    chore: add types

---
+++ whitelabelnews-state-manager.d.ts
@@ -0,0 +1,10 @@
+import {
+  store as exportedStore,
+  IRootState as exportedIRootState,
+  FETCH_NEWS as exportedFETCH_NEWS,
+} from './src';
+declare module 'whitelabelnews-state-manager' {
+  export const store: typeof exportedStore;
+  export type IRootState = exportedIRootState;
+  export const FETCH_NEW: exportedFETCH_NEWS;
+}
